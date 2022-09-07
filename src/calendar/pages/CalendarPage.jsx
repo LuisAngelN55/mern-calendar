4df103f3d34329e43ece44ae7ccde32d90977959
LuisMoov <lang@zuntrix.com>
2022-09-07 17:18:32 -0500

    347. Escuchar eventos del calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,9 +2,10 @@ import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { addHours,  } from "date-fns";
-import { Navbar } from "../components/Navbar";
+import { Navbar, CalendarEvent } from "../";
 
 import { localizer, getMessagesES } from '../../helpers';
+import { useState } from 'react';
 
 const events = [{
   title: 'Cumpleaños del jefe',
@@ -21,8 +22,9 @@ const events = [{
 
 export const CalendarPage = () => {
 
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week' );
+
   const eventStyleGetter = ( event, start, end, isSelected ) => {
-    console.log( { event, start, end, isSelected } );
 
     const style = {
       backgroundColor: '#347CF7',
@@ -34,7 +36,20 @@ export const CalendarPage = () => {
     return {
       style
     };
+  }
 
+
+  const onSelect = ( event ) => {
+    console.log( { Click: event });
+  }
+
+  const onDoubleClick = ( event ) => {
+    console.log( { DoubleClick: event });
+  }
+
+  const onViewChanged = ( event ) => {
+    localStorage.setItem( 'lastView', event );
+    setLastView( event );
   }
 
 
@@ -46,11 +61,18 @@ export const CalendarPage = () => {
         culture= 'es'
         localizer={ localizer }
         events={ events }
+        defaultView= { lastView }
         startAccessor="start"
         endAccessor="end"
-        style={{ height: 'calc( 100vh - 80px )' }}
+        style={{ height: 'calc( 100vh - 80px)' }}
         messages = { getMessagesES() }
         eventPropGetter = { eventStyleGetter }
+        components= {{
+          event: CalendarEvent
+        }}
+        onDoubleClickEvent = {onDoubleClick}
+        onSelectEvent = {onSelect}
+        onView = {onViewChanged}
       />
 
     </>
