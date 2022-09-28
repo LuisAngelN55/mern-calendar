23e7bd2d0ff4524c7a35539e22c583ccac10a053
LuisMoov <lang@zuntrix.com>
2022-09-28 17:50:56 -0500

    419. Mostrar eventos de la base de datos

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -10,7 +11,7 @@ import { useUiStore, useCalendarStore } from '../../hooks';
 
 export const CalendarPage = () => {
 
-  const { events, setActiveEvent } = useCalendarStore();
+  const { events, setActiveEvent, startLoadingEvents } = useCalendarStore();
   const { openDateModal } = useUiStore();
   const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week' );
 
@@ -42,6 +43,12 @@ export const CalendarPage = () => {
     localStorage.setItem( 'lastView', event );
     setLastView( event );
   }
+
+
+
+  useEffect(() => {
+    startLoadingEvents();
+  }, [])
 
 
   return (
